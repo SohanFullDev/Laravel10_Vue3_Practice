00ef47cccaf057ada46f5c4a2d522ad27f26f7bf
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-08 11:38:28 +0600

    5 updating data_s3_t17

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -2,27 +2,14 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 
-class UpdateTaskRequest extends FormRequest
+//use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends StoreTaskRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+
+
 }
