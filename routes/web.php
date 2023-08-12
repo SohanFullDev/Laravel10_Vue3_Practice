c8244823f4069279ff72907ca48909c6c06bdea2
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-12 11:58:28 +0600

    21_spa authentication-signing users out_s4_t21

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,5 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function(){
     Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
 });
